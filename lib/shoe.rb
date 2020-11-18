3ac825f6f078ca268d19ff82128d9fd7b7d58133
Cameron Bisca <cjbisca@gmail.com>
2020-11-17 19:18:20 -0500

    commit tree 57072d3082ca7fdd4cf0ae58e83935187bd54c7c

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_accessor :brand, :color, :size, :material
+    attr_reader :condition 
+    def initialize(brand)
+        @brand = brand 
+    end 
+
+    def cobble(shoe)
+        if shoe.condition == true 
+        puts "Your shoe is as good as new!"
+        end 
+    end 
+
+end 
